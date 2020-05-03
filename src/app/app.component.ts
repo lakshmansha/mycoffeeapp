3b018277ec6da11cbcad1606f8a424c38c1cdbd9
Lakshman.S <haranhari_38@outlook.com>
2020-05-03 17:39:42 +0530

    Course Completed. Added PWA on Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate, SwPush } from '@angular/service-worker';
+
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { GeolocationService } from './geolocation.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,56 @@ import { GeolocationService } from './geolocation.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private snackBar: MatSnackBar, private geolocation: GeolocationService) {}
+  VAPID_PUBLIC_KEY = 'replace-with-your-public-key';
+
+  constructor(private snackBar: MatSnackBar, private ngswUpdate: SwUpdate, private ngswPush: SwPush) { }
+
+  updateNetworkStatusUI() {
+    if (navigator.onLine) {
+      (document.querySelector('body') as any).style = '';
+    } else {
+      (document.querySelector('body') as any).style = 'filter: grayscale(1)';
+    }
+  }
+
+  SubcribeToPush() {
+    Notification.requestPermission(permission => {
+      if (permission === 'granted') {
+        this.ngswPush.requestSubscription({ serverPublicKey: this.VAPID_PUBLIC_KEY })
+          .then((registration: PushSubscription) => {
+            console.log(registration);
+            // TODO: Send that object to our server
+          });
+      }
+    });
+  }
 
   ngOnInit() {
+
+    // Checking SW Update Status
+    this.ngswUpdate.available.subscribe(update => {
+      if (update.type === 'UPDATE_AVAILABLE') {
+        const sb = this.snackBar.open('There is an Update available', 'Install Now', { duration: 4000 });
+        sb.onAction().subscribe(() => {
+          this.ngswUpdate.activateUpdate().then(event => {
+            console.log('The App was updated');
+            location.reload();
+          });
+        });
+      }
+    });
+    this.ngswUpdate.checkForUpdate();
+
+    // Checking Network Status
+    this.updateNetworkStatusUI();
+    window.addEventListener('online', this.updateNetworkStatusUI);
+    window.addEventListener('offline', this.updateNetworkStatusUI);
+
+    // Checking Installation Status
     if ((navigator as any).standalone === false) {
       // This is an iOS device and we are in the browser
       this.snackBar.open('You can add this PWA to the Home Screen',
-      '', { duration: 3000 } );
+        '', { duration: 3000 });
     }
 
     if ((navigator as any).standalone === undefined) {
@@ -25,7 +70,7 @@ export class AppComponent implements OnInit {
         window.addEventListener('beforeinstallprompt', event => {
           event.preventDefault();
           const sb = this.snackBar.open('Do you want to install this App?', 'Install',
-            {duration: 5000 });
+            { duration: 5000 });
 
           sb.onAction().subscribe(() => {
             (event as any).prompt();
@@ -41,6 +86,5 @@ export class AppComponent implements OnInit {
         });
       }
     }
-
   }
 }
